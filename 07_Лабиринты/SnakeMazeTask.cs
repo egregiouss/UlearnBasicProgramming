c8e039acdb7e67452f4b3570af8d0305b4797730
GZProgramming <82836533+GZProgramming@users.noreply.github.com>
2021-04-20 18:29:55 +0100

    Update SnakeMazeTask.cs

--- 07_Лабиринты/SnakeMazeTask.cs
+++ 07_Лабиринты/SnakeMazeTask.cs
@@ -1,9 +1,24 @@
-﻿namespace Mazes
+namespace Mazes
 {
-	public static class SnakeMazeTask
-	{
-		public static void MoveOut(Robot robot, int width, int height)
-		{
-		}
-	}
+    public static class SnakeMazeTask
+    {
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            int i = 0;
+            while (!robot.Finished && i < 1000)
+            {
+                MoveTo(robot, Direction.Right, width - 3);
+                MoveTo(robot, Direction.Down, 2);
+                MoveTo(robot, Direction.Left, width - 3);
+                MoveTo(robot, Direction.Down, 2);
+                i++;
+            }
+        }
+		
+        public static void MoveTo(Robot robot, Direction direction, int path)
+        {
+            for (int i = 0; i < path && !robot.Finished; i++)
+                robot.MoveTo(direction);
+        }
+    }
 }
