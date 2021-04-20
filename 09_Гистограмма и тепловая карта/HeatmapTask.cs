77fe9875bc6bf678f38791becd9825e62310f797
GZProgramming <82836533+GZProgramming@users.noreply.github.com>
2021-04-20 18:53:44 +0100

    Add files via upload

---
+++ 09_Гистограмма и тепловая карта/HeatmapTask.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var days = Enumerable.Range(2, 30)
+                                   .Select(x => x.ToString())
+                                   .ToArray();
+            var months = Enumerable.Range(1, 12)
+                                   .Select(x => x.ToString())
+                                   .ToArray();
+            return new HeatmapData("Карта интенсивностей", GetBirthsPerDate(names), days, months);
+        }
+
+        public static double[,] GetBirthsPerDate(NameData[] names)
+        {
+            var birthsCounts = new double[30, 12];
+            foreach (var n in names)
+            {
+                if (n.BirthDate.Day != 1)
+                    birthsCounts[n.BirthDate.Day - 2, n.BirthDate.Month - 1]++;
+            }
+            return birthsCounts;
+        }
+    }
+}
---
+++ 09_Гистограмма и тепловая карта/HistogramTask.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var days = Enumerable.Range(1, 31)
+                                 .Select(x => x.ToString())
+                                 .ToArray();
+            return new HistogramData("Рождаемость людей с именем " + name, days, GetBirthsPerDay(names, name));
+        }
+
+        public static double[] GetBirthsPerDay(NameData[] names, string name)
+        {
+            var birthsCounts = new double[31];
+            foreach (var n in names)
+            {
+                if (n.Name == name && n.BirthDate.Day != 1)
+                    birthsCounts[n.BirthDate.Day - 1]++;
+            }
+            return birthsCounts;
+        }
+    }
+}
