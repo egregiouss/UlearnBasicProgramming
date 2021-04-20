33e238a81e11ca60275f4322a31e957391bc3440
GZProgramming <82836533+GZProgramming@users.noreply.github.com>
2021-04-20 18:29:02 +0100

    Update EmptyMazeTask.cs

--- 07_Лабиринты/EmptyMazeTask.cs
+++ 07_Лабиринты/EmptyMazeTask.cs
@@ -1,9 +1,17 @@
-﻿namespace Mazes
+namespace Mazes
 {
 	public static class EmptyMazeTask
 	{
 		public static void MoveOut(Robot robot, int width, int height)
 		{
-		}
-	}
+                MoveTo(robot, Direction.Right, width - 3);
+                MoveTo(robot, Direction.Down,  height - 3);
+        }
+
+        public static void MoveTo(Robot robot, Direction direction, int path)
+        {
+            for (int i = 0; i < path && !robot.Finished; i++)
+                robot.MoveTo(direction);
+        }
+    }
 }
