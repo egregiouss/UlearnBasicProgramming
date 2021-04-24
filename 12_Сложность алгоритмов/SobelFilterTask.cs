461dde87d2a859ce422a49abe4c8d73a3690c217
GZProgramming <82836533+GZProgramming@users.noreply.github.com>
2021-04-24 10:15:53 +0100

    Update SobelFilterTask.cs

--- 12_Сложность алгоритмов/SobelFilterTask.cs
+++ 12_Сложность алгоритмов/SobelFilterTask.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Recognizer
 {
@@ -9,20 +9,41 @@ namespace Recognizer
             var width = g.GetLength(0);
             var height = g.GetLength(1);
             var result = new double[width, height];
-            for (int x = 1; x < width - 1; x++)
-                for (int y = 1; y < height - 1; y++)
-                {
-                    // Вместо этого кода должно быть поэлементное умножение матриц sx и полученной транспонированием из неё sy на окрестность точки (x, y)
-                    // Такая операция ещё называется свёрткой (Сonvolution)
-                    var gx = 
-                        -g[x - 1, y - 1] - 2 * g[x, y - 1] - g[x + 1, y - 1] 
-                        + g[x - 1, y + 1] + 2 * g[x, y + 1] + g[x + 1, y + 1];
-                    var gy = 
-                        -g[x - 1, y - 1] - 2 * g[x - 1, y] - g[x - 1, y + 1] 
-                        + g[x + 1, y - 1] + 2 * g[x + 1, y] + g[x + 1, y + 1];
-                    result[x, y] = Math.Sqrt(gx * gx + gy * gy);
-                }
+            var sy = TransposeMatrix(sx);
+            var sxWidth = sx.GetLength(0);
+            
+			sxWidth = (sxWidth - 1) / 2;
+            for (int x = sxWidth; x < width - sxWidth; x++)
+                for (int y = sxWidth; y < height - sxWidth; y++)
+                    result[x, y] = GetСonvolution(g, sx, sy, x, y);
+                
             return result;
+        }
+		
+        public static double[,] TransposeMatrix(double[,] sx)
+        {
+            var width = sx.GetLength(0);
+            var height = sx.GetLength(1);
+            var sy = new double[height, width];
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                    sy[x, y] = sx[y, x];
+            return sy;
+        }
+		
+        public static double GetСonvolution(double[,] g, double[,] sx, double[,] sy, int x, int y)
+        {
+            var width = sx.GetLength(0);
+            var height = sx.GetLength(1);
+            var gx = 0.0;
+            var gy = 0.0;
+            for (var i = 0; i < width; i++)
+                for (var j = 0; j < height; j++)
+                {       
+                    gx += sx[i, j] * g[x + i - (width - 1) / 2, y + j - (height - 1) / 2];
+                    gy += sy[i, j] * g[x + i - (width - 1) / 2, y + j - (height - 1) / 2];
+                }
+            return Math.Sqrt(gx * gx + gy * gy);
         }
     }
 }
