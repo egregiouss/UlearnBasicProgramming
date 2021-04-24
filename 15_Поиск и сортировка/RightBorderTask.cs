c3b11cca3bfec9d998f76d436d7f00b1f1baaffc
GZProgramming <82836533+GZProgramming@users.noreply.github.com>
2021-04-24 10:36:09 +0100

    Update RightBorderTask.cs

--- 15_Поиск и сортировка/RightBorderTask.cs
+++ 15_Поиск и сортировка/RightBorderTask.cs
@@ -1,31 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-
 namespace Autocomplete
 {
     public class RightBorderTask
     {
-        /// <returns>
-        /// Возвращает индекс правой границы. 
-        /// То есть индекс минимального элемента, который не начинается с prefix и большего prefix.
-        /// Если такого нет, то возвращает items.Length
-        /// </returns>
-        /// <remarks>
-        /// Функция должна быть НЕ рекурсивной
-        /// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
-        /// </remarks>
         public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
         {
-            // IReadOnlyList похож на List, но у него нет методов модификации списка.
-            // Этот код решает задачу, но слишком неэффективно. Замените его на бинарный поиск!
-            for (int i = phrases.Count-1; i >= 0; i--)
+            while (left < right)
             {
-                if (string.Compare(prefix, phrases[i], StringComparison.OrdinalIgnoreCase) >= 0 
-                    || phrases[i].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
-                    return i + 1;
+                var middle = left + (right - left) / 2;
+                if (middle < 0) return right;
+                if (string.Compare(prefix, phrases[middle]) <= 0 && 
+					!phrases[middle].StartsWith(prefix))
+                    right = middle;
+                else left = middle + 1;
             }
-            return 0;
+            return right;
         }
     }
 }
