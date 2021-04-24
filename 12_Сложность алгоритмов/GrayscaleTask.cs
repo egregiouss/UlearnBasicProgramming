eae9148d9bf06ec52906a37e6f44a54525756899
GZProgramming <82836533+GZProgramming@users.noreply.github.com>
2021-04-24 10:10:19 +0100

    Update GrayscaleTask.cs

--- 12_Сложность алгоритмов/GrayscaleTask.cs
+++ 12_Сложность алгоритмов/GrayscaleTask.cs
@@ -1,26 +1,18 @@
-﻿namespace Recognizer
+namespace Recognizer
 {
-	public static class GrayscaleTask
+    public static class GrayscaleTask
+    {
+        public static double[,] ToGrayscale(Pixel[,] original)
 	{
-		/* 
-		 * Переведите изображение в серую гамму.
-		 * 
-		 * original[x, y] - массив пикселей с координатами x, y. 
-		 * Каждый канал R,G,B лежит в диапазоне от 0 до 255.
-		 * 
-		 * Получившийся массив должен иметь те же размеры, 
-		 * grayscale[x, y] - яркость пикселя (x,y) в диапазоне от 0.0 до 1.0
-		 *
-		 * Используйте формулу:
-		 * Яркость = (0.299*R + 0.587*G + 0.114*B) / 255
-		 * 
-		 * Почему формула именно такая — читайте в википедии 
-		 * http://ru.wikipedia.org/wiki/Оттенки_серого
-		 */
-
-		public static double[,] ToGrayscale(Pixel[,] original)
-		{
-			return new double[original.GetLength(0), original.GetLength(1)];
-		}
+            var weight = original.GetLength(0);
+            var height = original.GetLength(1);
+            var grayscale = new double[weight, height];
+            for (var x = 0; x < weight; x++)
+                for (var y = 0; y < height; y++)
+                    grayscale[x, y] = (0.299 * original[x, y].R +
+                                       0.587 * original[x, y].G + 
+                                       0.114 * original[x, y].B) / 255;
+            return grayscale;
 	}
+    }
 }
