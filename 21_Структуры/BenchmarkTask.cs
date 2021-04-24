776a4ea812c8ff74fad0a5542c0c57933ad143f7
GZProgramming <82836533+GZProgramming@users.noreply.github.com>
2021-04-24 11:26:06 +0100

    Update BenchmarkTask.cs

--- 21_Структуры/BenchmarkTask.cs
+++ 21_Структуры/BenchmarkTask.cs
@@ -1,21 +1,92 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 using NUnit.Framework;
 
 namespace StructBenchmarking
 {
     public class Benchmark : IBenchmark
-	{
+    {
         public double MeasureDurationInMs(ITask task, int repetitionCount)
         {
-            GC.Collect();                   // Эти две строчки нужны, чтобы уменьшить вероятность того,
-            GC.WaitForPendingFinalizers();  // что Garbadge Collector вызовется в середине измерений
-                                            // и как-то повлияет на них.
-               
-			throw new NotImplementedException();
-		}
-	}
+            GC.Collect();                   
+            GC.WaitForPendingFinalizers();
+            var watch = new Stopwatch();
+            task.Run();
+            watch.Start();
+            for (var i = 0; i < repetitionCount; i++)
+            {
+                task.Run();
+            }
+            watch.Stop();
+            return (double)watch.ElapsedMilliseconds / repetitionCount;
+        }
+    }
+
+    public class StrBuilder : ITask
+    {
+        public char Letter {get; set;}
+        public int RepetitionCount { get; set; }
+        public StrBuilder(char letter, int repetitionCount)
+        {
+            Letter = letter;
+            RepetitionCount = repetitionCount;
+        }
+
+        public string RunStrBuilderAppend() 
+        {
+            var builder = new StringBuilder();
+            for (int i = 0; i < RepetitionCount; i++)
+            {
+                builder.Append(Letter);
+            }
+            return builder.ToString();
+        }
+
+        public void RunTestStrBuilderAppend()
+        {
+            var builder = new StringBuilder();
+            for (int i = 0; i < 10; i++)
+            {
+                builder.Append(Letter);
+            }
+            builder.Clear();
+        }
+		
+        public void Run() 
+        {
+            RunTestStrBuilderAppend();
+            RunStrBuilderAppend(); 
+        }
+    }
+
+    public class StringConstructor : ITask
+    {
+        public char Letter { get; set; }
+        public int RepetitionCount { get; set; }
+        public StringConstructor(char letter, int repetitionCount)
+        {
+            Letter = letter;
+            RepetitionCount = repetitionCount;
+        }
+
+        public string RunStringConstructor()
+        {
+            return new string(Letter, RepetitionCount);
+        }
+
+        public void RunTestConstructor()
+        {
+             new string('a', 10);
+        }
+		
+        public void Run() 
+        {
+            RunTestConstructor();
+            RunStringConstructor(); 
+        }
+    }
 
     [TestFixture]
     public class RealBenchmarkUsageSample
@@ -23,7 +94,12 @@ namespace StructBenchmarking
         [Test]
         public void StringConstructorFasterThanStringBuilder()
         {
-            throw new NotImplementedException();
+            var benchmark = new Benchmark();
+            var strConstructor = new StringConstructor('a', 100000);
+            var strBuilder = new StrBuilder('a', 100000);
+            var builderTime = benchmark.MeasureDurationInMs(strBuilder, 20);
+            var constructorTime = benchmark.MeasureDurationInMs(strConstructor, 20);
+            Assert.Less(constructorTime, builderTime);
         }
     }
 }
