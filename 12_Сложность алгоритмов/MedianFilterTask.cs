b649eb289c174d311673c9931bc88e7e0c8c6ad8
GZProgramming <82836533+GZProgramming@users.noreply.github.com>
2021-04-24 10:12:56 +0100

    Update MedianFilterTask.cs

--- 12_Сложность алгоритмов/MedianFilterTask.cs
+++ 12_Сложность алгоритмов/MedianFilterTask.cs
@@ -1,21 +1,43 @@
-﻿using System.Linq;
-
+using System;
+using System.Collections.Generic;
 namespace Recognizer
 {
-	internal static class MedianFilterTask
+    internal static class MedianFilterTask
 	{
-		/* 
-		 * Для борьбы с пиксельным шумом, подобным тому, что на изображении,
-		 * обычно применяют медианный фильтр, в котором цвет каждого пикселя, 
-		 * заменяется на медиану всех цветов в некоторой окрестности пикселя.
-		 * https://en.wikipedia.org/wiki/Median_filter
-		 * 
-		 * Используйте окно размером 3х3 для не граничных пикселей,
-		 * Окно размером 2х2 для угловых и 3х2 или 2х3 для граничных.
-		 */
 		public static double[,] MedianFilter(double[,] original)
 		{
-			return original;
-		}
-	}
+            var width = original.GetLength(0);
+            var height = original.GetLength(1);
+            var medianArray = new double[width, height];
+            for (var x = 0; x < width; x++)
+                for (var y = 0; y < height; y++)
+                    medianArray[x, y] = MedianBlock(original, x, y);
+            return medianArray;
+        }
+
+        public static double MedianBlock(double[,] original, int x, int y)
+        {
+            var list = new List<double>();
+            var width  = original.GetLength(0);
+            var height = original.GetLength(1);
+            var countRows    = 3;
+            var countColumns = 3;
+            if (x < 1 || x + 1 == width ) countRows--;
+            if (y < 1 || y + 1 == height) countColumns--;
+			if (x >= 1) x--;
+            if (y >= 1) y--;
+            for (var i = 0; i < countRows && i < width; i++)
+                for (var j = 0; j < countColumns && j < height; j++)
+                    list.Add(original[x + i, y + j]);
+            list.Sort();
+			return list.Count < 2 ? list[0] : GetMedianOfArray(list);
+        }
+  
+        public static double GetMedianOfArray(List<double> list)
+        {
+            if (list.Count % 2 == 0)
+                return (list[(int)(list.Count / 2) - 1] + list[(int)(list.Count / 2)]) / 2;
+            return list[(int)(list.Count / 2)];  
+        }
+    }
 }
