0267734805fddaee2be980cdc3e681a3345588fb
GZProgramming <82836533+GZProgramming@users.noreply.github.com>
2021-04-24 11:26:54 +0100

    Update ExperimentsTask.cs

--- 21_Структуры/ExperimentsTask.cs
+++ 21_Структуры/ExperimentsTask.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace StructBenchmarking
 {
@@ -7,33 +8,44 @@ namespace StructBenchmarking
         public static ChartData BuildChartDataForArrayCreation(
             IBenchmark benchmark, int repetitionsCount)
         {
-            var classesTimes = new List<ExperimentResult>();
-            var structuresTimes = new List<ExperimentResult>();
-            
-            //...
-
             return new ChartData
             {
                 Title = "Create array",
-                ClassPoints = classesTimes,
-                StructPoints = structuresTimes,
+                ClassPoints = Constants.FieldCounts.SelectMany(x => 
+				 BuildChartData(benchmark, x, new ClassArrayCreationTask(x), repetitionsCount))
+                                                   .ToList(),
+                StructPoints = Constants.FieldCounts.SelectMany(x => 
+				 BuildChartData(benchmark, x, new StructArrayCreationTask(x), repetitionsCount))
+                                                   .ToList(),
             };
         }
 
         public static ChartData BuildChartDataForMethodCall(
             IBenchmark benchmark, int repetitionsCount)
         {
-            var classesTimes = new List<ExperimentResult>();
-            var structuresTimes = new List<ExperimentResult>();
-            
-            //...
-
             return new ChartData
             {
                 Title = "Call method with argument",
-                ClassPoints = classesTimes,
-                StructPoints = structuresTimes,
+                ClassPoints = Constants.FieldCounts.SelectMany(x => 
+				 BuildChartData(benchmark, x, new MethodCallWithClassArgumentTask(x), repetitionsCount))
+                                                   .ToList(),
+                StructPoints = Constants.FieldCounts.SelectMany(x => 
+				 BuildChartData(benchmark, x, new MethodCallWithStructArgumentTask(x), repetitionsCount))
+                                                   .ToList(),
             };
+        }
+
+        public static List<ExperimentResult> BuildChartData(
+            IBenchmark benchmark, int fieldCount, 
+            ITask obj, int repetitionsCount)
+        {
+        	var resultTimes = new List<ExperimentResult>();
+
+            resultTimes.Add(new ExperimentResult
+                    (fieldCount, new Benchmark().MeasureDurationInMs
+                    (obj, repetitionsCount)));
+            
+        	return resultTimes;
         }
     }
 }
