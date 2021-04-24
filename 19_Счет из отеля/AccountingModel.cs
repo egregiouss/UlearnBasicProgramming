5edb50dc5a8ca533cd1f6dccd62e0f2c24695d2f
GZProgramming <82836533+GZProgramming@users.noreply.github.com>
2021-04-24 11:13:40 +0100

    Update AccountingModel.cs

--- 19_Счет из отеля/AccountingModel.cs
+++ 19_Счет из отеля/AccountingModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +6,71 @@ using System.Threading.Tasks;
 
 namespace HotelAccounting
 {
-    //создайте класс AccountingModel здесь
+    public class AccountingModel : ModelBase
+    {
+        private double price;
+        private int nightsCount;
+        private double discount;
+        private double total;
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0) throw new ArgumentException();
+                price = value;
+                Notify(nameof(Price));
+                ChangeTotal();
+            }
+        }
+
+        public int NightsCount
+        {
+            get { return nightsCount; }
+            set
+            {
+                if (value <= 0) throw new ArgumentException();
+                nightsCount = value;
+                Notify(nameof(NightsCount));
+                ChangeTotal();
+            }
+        }
+
+        public double Discount
+        {
+            get { return discount; }
+            set
+            {
+                discount = value;
+                if (discount > 100) throw new ArgumentException();
+                Notify(nameof(Discount));
+                ChangeTotal();
+            }
+        }
+
+        public double Total
+        {
+            get { return total; }
+            set
+            {
+                total = value;
+                if (total < 0) throw new ArgumentException();
+                Notify(nameof(Total));
+                ChangeDiscount();
+            }
+        }
+
+        public void ChangeTotal()
+        {
+            total = price * nightsCount * (1 - discount / 100);
+            Notify(nameof(Total));
+        }
+
+        public void ChangeDiscount()
+        {
+            discount = (total / (price * nightsCount) - 1) * -100;
+            Notify(nameof(Discount));
+        }
+    }
 }
